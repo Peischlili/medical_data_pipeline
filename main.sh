d11436ecf3623b9a9a3e397d4f9948fc73a62e9b
Peischlili <peishan.caroline.li@gmail.com>
2022-01-31 00:12:45 +0100

    changed main.sh

---
+++ main.sh
@@ -0,0 +1,8 @@
+docker exec -it ctn_restit /bin/bash
+
+python3 /home/med_project/main.py
+
+exit
+
+#Tester si le container a bien démarré
+docker ps -a
--- pubmed_restit.sh
+++ pubmed_restit.sh
@@ -5,12 +5,20 @@ mkdir tempdir/data
 # Copier fichiers à tempdir
 cp -r data/* tempdir/data/.
 cp pubmed_restitution.py tempdir/.
+cp components.py tempdir/
+cp processing_fct.py tempdir/
+cp main.py tempdir/
 
 # Création du fichier Dockerfile
-echo "FROM python" > tempdir/Dockerfile
+echo "FROM python" > tempdir/Dockerfile	
 echo "RUN pip3 install pandas" >> tempdir/Dockerfile
 echo "RUN pip3 install json" >> tempdir/Dockerfile
+echo "RUN pip3 install re" >> tempdir/Dockerfile
+echo "RUN pip3 install sys" >> tempdir/Dockerfile
 echo "COPY  pubmed_restitution.py /home/med_project/" >> tempdir/Dockerfile
+echo "COPY  components.py /home/med_project/" >> tempdir/Dockerfile
+echo "COPY  processing_fct.py /home/med_project/" >> tempdir/Dockerfile
+echo "COPY  main.py /home/med_project/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/med_project/pubmed_restitution.py" >> tempdir/Dockerfile
 
@@ -19,7 +27,7 @@ cd tempdir
 docker build -t pubmed_restit .
 
 #lancement du container  testrunning avec l'image testapp
-docker run -t -d -p 5050:5050 --name ctn_restit pubmed_restit
+docker run -t -d -p 5050:5050 --name test_restit pubmed_restit
 
 #Tester si le container a bien démarré
 docker ps -a
