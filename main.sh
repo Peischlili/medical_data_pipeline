9ba75394b57b84bda1df29f22d73a142bd3b6932
Peischlili <peishan.caroline.li@gmail.com>
2022-01-31 00:45:29 +0100

    change docker command

--- main.sh
+++ main.sh
@@ -1,8 +1,4 @@
-docker exec -it ctn_restit /bin/bash
-
-python3 /home/med_project/main.py
-
-exit
+docker exec -d ctn_restit python3 /home/med_project/main.py
 
 #Tester si le container a bien démarré
 docker ps -a
